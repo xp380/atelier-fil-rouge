b3a218ed8b983124bfcd3660a84a39ea3accf77e
xp380(Vincent Kouoï) <47360785+xp380@users.noreply.github.com>
2020-05-22 15:55:44 +0200

    Update conf.js

--- Rouge/server/routes/conf.js
+++ Rouge/server/routes/conf.js
@@ -3,7 +3,7 @@ const  mysql = require('mysql');
 const  connection = mysql.createConnection({
 host :  'localhost', // adresse du serveur
 user :  'root', // le nom d'utilisateur
-password :  'ARCOxp380', // le mot de passe
+password :  '', // le mot de passe
 database :  'Atelier_fil_rouge', // le nom de la base de données
 });
 
